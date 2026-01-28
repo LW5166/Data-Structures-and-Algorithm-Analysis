932f01c75a42a2399fc8286db27d6279ecae927f
LW5166 <liwhg86@gmail.com>
2026-01-28 19:50:02 +0800

    refactor：删去了冗余变量result。

--- Day02/P1_factorial.c
+++ Day02/P1_factorial.c
@@ -3,17 +3,15 @@
 long long factorial(int n){   /*递归计算阶乘*/
     long long result=1;
     if(n==0)
-        result = 1;
+        return 1;
     else if(n<0)
         return -1;
-    else{
-        result=n*factorial(n-1);
-    }
-    return result;
+    else
+        return n*factorial(n-1);
 }
 
 int main(){
     for(int i=0;i<20;i++)
-        printf("%lld\n",factorial(i));
+        printf("%d!=%lld\n",i,factorial(i));
     return 0;
 }
