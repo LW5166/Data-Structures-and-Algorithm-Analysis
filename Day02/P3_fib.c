63a3be4c30e6a4f9d4393ea8691c4f41cc2dfda4
LW5166 <liwhg86@gmail.com>
2026-01-28 20:14:29 +0800

    fix:修复了base case考虑不全，增加了时间计算体会效率问题

--- Day02/P3_fib.c
+++ Day02/P3_fib.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
+#include<time.h>
 
 long long fib(int n){   /*计算斐波那契数列*/
     long long result=0;
-    if(n==1) result=1;
-    else{
-        result=fib(n-1) + fib(n-2);
-    }
-    return result;
+    if(n==1 || n==2) return 1;
+    return fib(n-1) + fib(n-2);
 }
 
 int main(){
-    for(int i=1;i<40;i++)
-        printf("%lld",fib(i));
+    for(int i=40;i<100;i++){
+        clock_t start=clock();
+        printf("fib(%d)=%lld\n",i,fib(i));
+        clock_t end=clock();
+        int time=(end - start)/CLOCKS_PER_SEC;
+        printf("time=%d\n",time);
+    }
     return 0;
 }
 
