7e12a6f935f36a3de4614b23c28454e97b91ff77
LW5166 <liwhg86@gmail.com>
2026-01-28 19:51:17 +0800

    refactor:为增强对函数理解，不再使用printf函数

--- Day02/P2_print_out.c
+++ Day02/P2_print_out.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 
 void print_out(unsigned int n){     /*递归按位显示每个数字*/
-    if(n<10 && n>=0){
-        printf("%d",n);
+    if(n<10){
+        putchar(n+'0');
     }
     else{
         print_out(n/10);
-        printf("%d",n%10);
+        putchar(n%10+'0');
     }
 }
 
 int main(){
     unsigned int n;
-    for(n=0;n<100;n++){
+    for(n=100000;n<10000000;n++){
         print_out(n);
         printf("\n");
     }
